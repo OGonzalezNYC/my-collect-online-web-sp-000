a05302d9c9657c6c3562d8428c35bb9298ffaa7c
Omar Gonzalez <ogonzaleznyc@gmail.com>
2021-02-05 10:50:50 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,12 +1,12 @@
-# def my_collect(array)
-#   new_array = []
-#   i = 0 
-#   while i < array.length 
-#     new_array << yield(array[i])
-#     i += 1
-#   end 
-#   new_array
-# end
+def my_collect(array)
+  new_array = []
+  i = 0 
+  while i < array.length 
+    new_array << yield(array[i])
+    i += 1
+  end 
+  new_array
+end
 
 # def my_collect(array)
 #   i = 0
